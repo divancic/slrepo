e5f5c5a23f1d1b0fbce2cb672c5e2e6af533b9e1
Dorian Ivancic <dorian.ivancic@gmail.com>
2016-08-26 14:05:46 +0200

    add sabrina modul

---
+++ sabrina.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sabrina.h"
+
+int
+sabrina(void) {
+  printf ("Sabrinin modul!\n");
+}
---
+++ sabrina.h
@@ -0,0 +1,6 @@
+#ifndef _SABRINA_H
+#define _SABRINA_H
+
+int sabrina(void);
+
+#endif /* _SABRINA_H */
